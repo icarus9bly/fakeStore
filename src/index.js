d2c7bb4cb16935ad7410082d6fde198dd3ae4ab3
Nikhil Pratap Singh <pratapnikhil106@gmail.com>
2024-06-11 09:08:43 +0530

    Fetch Product from API and rendered into Home

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import "./index.css";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import ProductProvider from "./contexts/ProductContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ProductProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ProductProvider>
 );
